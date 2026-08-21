9f01f5d9d8a61486dab6931c148271f564a9cbb5
GitHub Action <action@github.com>
2026-08-21 17:33:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/supernovan/geminiagent/MyBundle.kt
+++ src/main/kotlin/com/github/supernovan/geminiagent/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.supernovan.geminiagent
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/supernovan/geminiagent/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/supernovan/geminiagent/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.supernovan.geminiagent.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.supernovan.geminiagent.MyBundle
+import com.github.supernovan.geminiagent.services.MyProjectService
 import javax.swing.JButton
 
 
